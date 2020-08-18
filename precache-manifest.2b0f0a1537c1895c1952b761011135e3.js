f80fd64f4c00fc626899cfec8a242e878e5e8407
ArmanKamali <arman.amirkamali@gmail.com>
2020-08-18 10:35:01 +0430

    Updates

---
+++ precache-manifest.2b0f0a1537c1895c1952b761011135e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49916dfded459348f5ad692b75cbaa5c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "368b2e961a7fa3724970",
+    "url": "/resume/static/css/main.05c047a4.chunk.css"
+  },
+  {
+    "revision": "87b74f1668110b076219",
+    "url": "/resume/static/js/2.070fef01.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/resume/static/js/2.070fef01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368b2e961a7fa3724970",
+    "url": "/resume/static/js/main.e47bf399.chunk.js"
+  },
+  {
+    "revision": "c3d826a2528bd1ccf190",
+    "url": "/resume/static/js/runtime-main.66be5c4d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/resume/static/media/logo.ee7cd8ed.svg"
+  }
+]);
